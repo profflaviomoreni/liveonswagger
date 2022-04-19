2f134131be714149236a25c2861ce8cfcffe273f
Flavio Moreni <proflavio.moreni@fiap.com.br>
2022-04-19 18:22:08 -0300

    add customer feat

---
+++ liveonswagger/Controllers/CustomerController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using liveonswagger.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace liveonswagger.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IList<CustomerGet>> Get()
+        {
+            IList<CustomerGet> customers = new List<CustomerGet>();
+            customers.Add(new CustomerGet()
+            {
+                FirstName = "José",
+                LastName = "Silva Jr",
+                Email = "jose@domain.com",
+                Active = true,
+                IdCustomer = 1,
+                CreatedAt = DateTime.Now
+            });
+
+            customers.Add(new CustomerGet()
+            {
+                FirstName = "Maria",
+                LastName = "Silva Filha",
+                Email = "maria@domain.com",
+                Active = true,
+                IdCustomer = 2,
+                CreatedAt = DateTime.Now
+            });
+
+
+            return Ok(customers);
+        }
+
+
+        [HttpGet("{id}" , Name = "GET")]
+        public ActionResult<CustomerGet> Get(int id)
+        {
+            var customer = new CustomerGet()
+            {
+                FirstName = "Maria",
+                LastName = "Silva Filha",
+                Email = "maria@domain.com",
+                Active = true,
+                IdCustomer = 2,
+                CreatedAt = DateTime.Now
+            };
+
+
+            return Ok(customer) ;
+        }
+
+
+        [HttpPost]
+        public ActionResult Post([FromBody] CustomerPost customer)
+        {
+            var customerGet = new CustomerGet()
+            {
+                FirstName = "Maria",
+                LastName = "Silva Filha",
+                Email = "maria@domain.com",
+                Active = true,
+                IdCustomer = 2,
+                CreatedAt = DateTime.Now
+            };
+
+            return CreatedAtRoute("Get",
+                new { id = customerGet.IdCustomer },
+                value: customerGet );
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] CustomerPut customer)
+        {
+            if (id == 3)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if ( id == 3)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ liveonswagger/Model/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace liveonswagger.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+        public bool Active { get; set; }
+
+    }
+
+
+    public class CustomerPut : Customer
+    {
+        public int IdCustomer { get; set; }
+        public String Password { get; set; }
+    }
+
+    public class CustomerPost : Customer
+    {
+        public String Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class CustomerGet : Customer
+    {
+        public int IdCustomer { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+
+}
