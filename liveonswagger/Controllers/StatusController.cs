ecf24b493c89eadd4942550aecc2d64421926fac
Flavio Moreni <proflavio.moreni@fiap.com.br>
2022-04-19 16:53:47 -0300

    inicial commit

---
+++ liveonswagger/Controllers/StatusController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace liveonswagger.Controllers
+{
+    [Route("api/[controller]")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ativo", "inativo" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "ativo";
+        }
+
+        
+    }
+}
